f79642054a057bafe46efa0d8cfb7519b57a6c6a
Charles Silva Batista Dos Santos <charles.santos@trusthub.com.br>
2019-01-09 16:14:43 -0200

    translate: adicionar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'siteazul';
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('pt');
+  }
+
+  public site__azul = 'site__azul';
 }
